a1c18837294b31fb7a7dd41eab1784e20fb8670a
ts-oh <tsohlove@gmail>
2021-09-16 14:50:37 +0900

    working caesar cipher as instructed from TOP curriculum

--- cc.rb
+++ cc.rb
@@ -1,30 +1,34 @@
-# Ruby Caesar Cipher Project
-require 'pry-byebug'
-
 # functions takes two parameters => 1. string of words and 2. number to shift
-def output(word, shift)
-  # map each character in to ASCII key and subtract 97 to get (a-z => 0-25)
-  parsed_word = word.downcase.bytes.map { |char| (char) - 97 }
-  p parsed_word
+def output(word, num)
+  # split string to characters to a new array
+  split_word_array = word.split('')
 
-=begin
-  1. caesar ciphere formula => y = (X + K) %mod 26
-  2. get the sum of charcter num (x) and number of shift (K)
-  3. get shifted character num Y by modulo26 of (X + K)
-  4. add by 97 to get ASCII key
-  5. return new array by map
-=end
-  shifted_word = parsed_word.map { |int| ((int + shift) % 26) + 97 }
+  # change characters to ASCII key and push to a new array
+  ascii_array = []
 
-  # map each ASCII key to character in the array
-  # return new array by map and join the characters in the array
-  to_parsed_num = shifted_word.map { |char| (char.ord.chr) }
-  to_parsed_num.join
+  split_word_array.each do |c|
+    c.ord
+    ascii_array.push(c.ord)
+  end
+
+  # shift ASCII key and map to a new array
+  shifted_ascii_array =
+    ascii_array.map do |n|
+      if n > 96 && n < 123
+        (((n - 97) + num) % 26) + 97
+      elsif n > 64 && n < 97
+        (((n - 65) + num) % 26) + 65
+      else
+        n
+      end
+    end
+
+  # map converted ASCII key to characters
+  encrypt_word = shifted_ascii_array.map { |n| n.chr }
+
+  # join characters in the array
+  encrypt_word.join
 end
 
-p 'please enter a word you like to encrypt below: '
-p word = gets.chomp.to_s
-p 'please enter number for the shift below: '
-p shift = gets.chomp.to_i
-p 'your encryption is: '
-p output(word, shift)
+# expected output => "Bmfy f xywnsl!"
+p output('What a string!', 5)
